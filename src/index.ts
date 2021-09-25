9f185d9193a354936e71d5f6e05c1080c60da247
hbybyyang <2451759073@qq.com>
2021-09-25 12:48:10 +0800

    debug

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,10 @@ export default function fun(cmd: string[], opt?: child_process.ExecSyncOptionsWi
         throw [ex.stdout, ex.stderr]
             .map((a) => a.replace(/\r/g, ''))
             .map((a) => a.replace(/\n/g, ''))
+            .map((a) => a.replace(/Active code page: 65001/g, ''))
+            .map((a) => a.trim())
             .filter((a) => a != null)
             .filter((a) => a != '')
-            .filter((a) => a != '\n')
-            .filter((a) => a != 'Active code page: 65001')
             .join('\n')
     }
 
