2bb4e5a8bf2841b50b3010209e8b1719ec451df2
hbybyyang <2451759073@qq.com>
2021-09-25 12:28:36 +0800

    出错的时候还是直接返回字符串把

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ export default function fun(cmd: string[], opt?: child_process.ExecSyncOptionsWi
         })
     } catch (e) {
         var ex = e as any
-        throw 字符串转数组([ex.stdout, ex.stderr].join('\n'))
+        throw [ex.stdout, ex.stderr].join('\n')
     }
 
     return 字符串转数组(结果)
